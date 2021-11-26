af94ea5e8045fb1e48c87f9902ca8ec4fa9b2696
mahmoodi <alimahmudi.alami@gmail.com>
2021-11-27 00:09:12 +0330

    show current date

--- src/components/SmallCalendar.tsx
+++ src/components/SmallCalendar.tsx
@@ -1,25 +1,30 @@
 import moment from "jalali-moment";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { getDaysOfMonth } from "../utility/createMonthMatrix";
 // moment.locale("en");
 
 export default function SmallCalendar() {
   const [state, setState] = useState(moment());
+  const [matrix, setMatrix] = useState<any>([]);
   const nextMonth = () => {
     setState(state.clone().add(1, "jMonth"));
   };
   const prevMonth = () => {
     setState(state.clone().subtract(1, "jMonth"));
   };
-  const matrix = getDaysOfMonth(state);
+  useEffect(() => {
+    setMatrix(getDaysOfMonth(state));
+  }, [state]);
+
   return (
     <div className="small-calendar-wrapper">
       <div className="small-calendar-header">
+        {state.format("jMMM jYYYY")}
         <button onClick={nextMonth}>next</button>
         <button onClick={prevMonth}>prev</button>
       </div>
       <div className="small-calendar-content">
-        {matrix.map((item, index1) => {
+        {matrix.map((item: any, index1: any) => {
           return (
             <div key={index1} className="small-calendar-row">
               {item.map((item2: any, index2: any) => {
--- src/utility/createMonthMatrix.ts
+++ src/utility/createMonthMatrix.ts
@@ -3,15 +3,13 @@ import moment from "jalali-moment";
 
 function chunkArray(myArray: any, chunk_size: any) {
   var results = [];
-
   while (myArray.length) {
     results.push(myArray.splice(0, chunk_size));
   }
-
   return results;
 }
 
-export function getDaysOfMonth(month = moment(), isGregorian = false) {
+export function getDaysOfMonth(month: any, isGregorian = false) {
   const days = [];
   const monthFormat = isGregorian ? "month" : "jMonth";
   const dayOffset = isGregorian ? 0 : 1;
@@ -28,5 +26,3 @@ export function getDaysOfMonth(month = moment(), isGregorian = false) {
   }
   return chunkArray(days, 7);
 }
-
-getDaysOfMonth();
